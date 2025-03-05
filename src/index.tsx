05bd244f699496a438cdfbc818ab19f153ce7055
Maxim Litonov <ReksesZz@yandex.ru>
2025-03-05 16:25:00 +0500

    Open, close (+ outside) form

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties } from 'react';
+import { StrictMode, CSSProperties, useState } from 'react';
 import clsx from 'clsx';
 
 import { Article } from './components/article/Article';
@@ -13,6 +13,12 @@ const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
 const App = () => {
+	const [isFormOpen, setIsFormOpen] = useState(false);
+
+	const toggleForm = () => {
+		setIsFormOpen((prev) => !prev);
+	};
+
 	return (
 		<main
 			className={clsx(styles.main)}
@@ -25,7 +31,7 @@ const App = () => {
 					'--bg-color': defaultArticleState.backgroundColor.value,
 				} as CSSProperties
 			}>
-			<ArticleParamsForm />
+			<ArticleParamsForm isOpen={isFormOpen} onArrowClick={toggleForm} />
 			<Article />
 		</main>
 	);
